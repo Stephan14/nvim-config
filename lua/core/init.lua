d1230ac3c9179299f941626a636a1c48e2c581b3
zoushengfu <zoushengfu@bytedance.com>
2023-03-14 20:36:54 +0800

    保存时支持自动format

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,6 +1,8 @@
 -- basicsinit
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')
+vim.cmd('autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()')
+
 vim.opt.number         = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors  = true
