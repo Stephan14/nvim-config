c3fd2a0c0490c10fe011bbe135537c5e96086163
zoushengfu <zoushengfu@bytedance.com>
2023-03-23 13:26:01 +0800

    remove sourcekit server

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -118,7 +118,7 @@ function M.config()
     -- nvim-lspconfig config
     -- List of all pre-configured LSP servers:
     -- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    local servers = { 'clangd', 'rust_analyzer', 'pylsp', 'lua_ls', 'sourcekit', 'gopls'}
+    local servers = { 'clangd', 'rust_analyzer', 'pylsp', 'lua_ls', 'html', 'cssls', 'gopls' }
     for _, lsp in pairs(servers) do
         require('lspconfig')[lsp].setup {}
     end
