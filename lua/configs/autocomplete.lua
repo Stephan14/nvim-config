35c2fc4915019081893a7499ecd66976fa9f301b
Stephan14 <zsf657135507@gmail.com>
2025-11-24 00:15:50 +0800

    fix lua

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -6,7 +6,7 @@ function M.config()
     -- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     
     -- Servers without extra config
-    local servers = { "pylsp", "html", "zls", "clangd", "gopls", 'sumneko_lua', 'sourcekit' }
+    local servers = { "pylsp", "html", "zls", "clangd", "gopls", 'lua_ls', 'sourcekit' }
     local lspconfig = require("lspconfig")
     for _, lsp in pairs(servers) do
         lspconfig[lsp].setup {}
