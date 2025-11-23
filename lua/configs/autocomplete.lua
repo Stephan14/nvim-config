85bc7a01ff5ee0f93ad3101ed37dcdc9ca981851
Stephan14 <zsf657135507@gmail.com>
2025-11-24 00:02:26 +0800

    修改lspsaga的初始化函数

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -24,6 +24,23 @@ function M.config()
         cmd = {"clangd", "-header-insertion=never" }
     }
 
+    require("lspsaga").setup({
+        ui = {
+            -- This option only works in Neovim 0.9
+            title = true,
+            -- Border type can be single, double, rounded, solid, shadow.
+            border = "single",
+            winblend = 0,
+            expand = " ",
+            collapse = " ",
+            code_action = " ",
+            incoming = " ",
+            outgoing = " ",
+            hover = ' ',
+            kind = {},
+        },
+    })
+
     -- Setup nvim-cmp.
     local cmp = require("cmp")
     local luasnip = require("luasnip")
